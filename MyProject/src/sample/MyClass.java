d9de1d427d489ebffb091659ff10d486164269ae
Deepak kumar S <deepak.s2@mindtree.com>
2019-09-05 23:53:04 +0530

    added wishes

--- MyProject/src/sample/MyClass.java
+++ MyProject/src/sample/MyClass.java
@@ -5,7 +5,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-
+		System.out.println("hai");
+		System.out.println("how are you");
 		System.out.println("Deepak");
 	}
 
