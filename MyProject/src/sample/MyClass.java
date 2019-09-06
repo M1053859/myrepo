8b15f5d3e5c425e9ef700fd8276ef55343d85916
pavan <M1053593@mindtree.com>
2019-09-06 10:07:35 +0530

    edited by pavan

--- MyProject/src/sample/MyClass.java
+++ MyProject/src/sample/MyClass.java
@@ -7,16 +7,18 @@ public class MyClass {
 		simple s=new simple("Gopal", 1053, 14000);
 		simple p=new simple("deepak", 1054, 15000);
 
-		simple d=new simple("pavan", 1055, 16000);
+		simple d=new simple("pavan",1055, 16000);
 		simple f=new simple("nagi", 1056, 18000);
 
 		simple g=new simple("nithesh", 1057, 17000);
 		simple h=new simple("Sai", 1059, 20000);
 		
 		
+		
+		
 		System.out.println(s.getEmployeeId()+","+s.getEmployeeName()+","+s.getEmployeesalary());
 		
-
+		System.out.println(p.getEmployeeId()+","+p.getEmployeeName()+","+p.getEmployeesalary());
 
 		
 		
