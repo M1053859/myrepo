a021a4baf6007240b35b7a600b0b143f9394d84d
deepak kumar s <deepak.s2@mindtree.com>
2019-09-09 08:55:45 +0530

    jms ADDED

---
+++ Jmsdemo/src/main/java/com/mindtree/Jmsdemo/JmsdemoApplication.java
@@ -0,0 +1,30 @@
+package com.mindtree.Jmsdemo;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+
+@SpringBootApplication
+@EnableJms
+public class JmsdemoApplication {
+   @Bean
+   public JmsListenerContainerFactory<?>jmsListenerContainerFactory(ConnectionFactory connectionFactory,DefaultJmsListenerContainerFactoryConfigurer configurer) {
+      DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+      configurer.configure(factory, connectionFactory);
+      return factory;
+   }
+
+   public static void main(String[] args) throws Exception{
+      ConfigurableApplicationContext context = SpringApplication.run(JmsdemoApplication.class,args);
+      MessageSender sender=context.getBean("messageSender",MessageSender.class);
+      sender.send("msgQueue","Hi! How are you?");
+      context.close();
+   }
+}
---
+++ Jmsdemo/src/main/java/com/mindtree/Jmsdemo/MessageReceiver.java
@@ -0,0 +1,13 @@
+package com.mindtree.Jmsdemo;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageReceiver {
+   @JmsListener(destination = "msgQueue", containerFactory = "jmsListenerContainerFactory")
+   public void receiveMessage(String msg) {
+      System.out.println("Message Received\n\n");
+      System.out.println("Message received from Queue: "+msg);
+   }
+}
