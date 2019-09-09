a9242c0b092b7adb58f2301bd1cace4276329e50
m1053734 <gopalkrishna.shanbhag2@mindtree.com>
2019-09-09 08:21:19 +0530

    final modify

--- MyProject/src/sample/Final.java
+++ MyProject/src/sample/Final.java
@@ -7,6 +7,7 @@ public class Final {
 public static void display(simple d) {
 	System.out.println(d.getEmployeeId());
 	System.out.println(d.getEmployeeName());
+	System.out.println(d.getEmployeesalary());
 }
 
 }
